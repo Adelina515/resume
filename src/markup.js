20875d2be9b53f373019c7a6d3bb35f7ae3de013
Adelina515 <adelgodlevska@gmail.com>
2024-08-08 00:22:02 +0300

    add join

--- src/markup.js
+++ src/markup.js
@@ -3,20 +3,22 @@ import { projectList } from "./projectList.js";
 console.log("projectList", projectList);
 const listProject = document.querySelector(".project");
 
-const markup = projectList.map(
-  ({ img, alt, text, stack, linkPage, linkGit, project }) => `
+const markup = projectList
+  .map(
+    ({ img, alt, text, stack, linkPage, linkGit, project }) => `
 <li class="list">
      <h3>${alt}</h3>
      <img src=${img} alt=${alt} width="210"/>
      <p>${text}</p>
-     <span>${stack}</span>
+     <span>${stack.join(", ")}</span>
      <span>${project.projectType}/ ${project.role}</span>
      <ul>
-        <li class="link"><a href=${linkPage}>link/Page</a></li>
-        <li class="link"><a href=${linkGit}>link/Git</a></li>
+        <li class="list"><a href=${linkPage}>link/Page</a></li>
+        <li class="list"><a href=${linkGit}>link/Git</a></li>
      </ul>
 </li>
 `
-);
+  )
+  .join("");
 
 listProject.insertAdjacentHTML("beforeend", markup);
